bab926bfba4f73ec69ec917c7251704c865f507b
Sonali Burman <sonali.b.020200@gmail.com>
2024-03-26 22:10:59 +0530

    movies

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -31,7 +33,7 @@ const App = () => {
     <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
     </div>
     <div className='row'>
-    <MovieList movies = {movies}/>
+    <MovieList movies = {movies} favouriteComponent={AddFavourites}/>
     </div>
    
   </div>
--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 const MovieList = (props) => {
+    const FavouriteComponent = props.favouriteComponent;
     return (
         <>
         {props.movies.map((movie, index)=> (
-             <div className="d-flex justify-content-start m-3">
+             <div className="image-container d-flex justify-content-start m-3">
              <img src={movie.Poster} alt="movie"></img>
+             <div className="overlay d-flex align-items-center justify-content-center">
+                <FavouriteComponent/>
+             </div>
          </div>
         ))}
         </>
